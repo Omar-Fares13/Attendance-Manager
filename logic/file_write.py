bd694a6e5375517dc82c166be081e1efde1a7779
AhmedM <ahmedmahmoud15520034@gmail.com>
2025-05-12 01:42:17 +0300

    2nd report view ya rab n5ls

--- logic/file_write.py
+++ logic/file_write.py
@@ -28,15 +28,15 @@ def get_notes(std):
 
 def format_students(students):
     formated_students = [
-        [std.name,
-         std.seq_number,
+        [std.seq_number,
+         std.name,
          std.national_id,
          std.faculty.name,
          get_warnings(std),
          len(std.attendance),
-         len(std.attendance) - 12,
+         12 - len(std.attendance),
+         "ناجح" if len(std.attendance) >= 10 else "راسب",
          get_notes(std),
-         "ناجح" if len(std.attendance) >= 10 else "راسب"
         ]
         for std in students
     ]
